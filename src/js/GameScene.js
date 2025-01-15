7b54bdc2e654a14546adcbabd338635e68119379
Supriy0-M <74642164+Supriy0-M@users.noreply.github.com>
2025-01-15 21:34:24 +0530

    gitignore added
    gitignore added

--- src/js/GameScene.js
+++ src/js/GameScene.js
@@ -1,20 +1,23 @@
 import { Constant } from "./Constant.js";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import CANNON from "cannon";
 
 export default class GameScene {
   constructor() {
     this.texture = null;
     this.stack = [];
+
+    this.overhangs = [];
   }
-  AddLayer(x, z, width, depth, direction) {
+  AddLayer(x, z, width, depth, direction,false) {
     const y = Constant.boxHeight * this.stack.length;
 
     const layer = this.generateBox(x, y, z, width, depth);
     layer.direction = direction;
     this.stack.push(layer);
   }
-  generateBox(x, y, z, width, depth) {
+  generateBox(x, y, z, width, depth, falls) {
     const geometry = new THREE.BoxGeometry(width, Constant.boxHeight, depth);
     const color = new THREE.Color(
       `hsl(${30 + this.stack.length * 4},100%,50%)`
@@ -25,13 +28,38 @@ export default class GameScene {
 
     Constant.game.add(mesh);
 
+    //cannon part
+    const shape = new CANNON.Box(
+      new CANNON.Vec3(width / 2, Constant.boxHeight / 2, depth / 2)
+    );
+    let mass = falls ? 5 : 0;
+    const body = new CANNON.body({ mass, shape });
+    body.position.set(x, y, z);
+    Constant.world.addBody(body);
+
     return {
       threejs: mesh,
+      cannonjs: body,
       width,
       depth,
     };
   }
+
+  AddOverHang(x, z, width, depth) {
+    const y = Constant.boxHeight * (this.stack.length - 1);
+    const overhang = this.generateBox(x, y, z, width, depth);
+    this.overhangs.push(overhang);
+  }
   RandomNumberGenerator(_min, _max) {
     return Math.random() * (_max - _min) + _min;
+  }
+  updatePhysics(timePassed) {
+    Constant.world.step(timePassed / 1000); // Step the physics world
+
+    // Copy coordinates from Cannon.js to Three.js
+    this.overhangs.forEach((element) => {
+      element.threejs.position.copy(element.cannonjs.position);
+      element.threejs.quaternion.copy(element.cannonjs.quaternion);
+    });
   }
 }
--- src/script.js
+++ src/script.js
@@ -2,10 +2,21 @@ import "./style.css";
 import * as THREE from "three";
 import { Constant } from "./js/Constant.js";
 import GameScene from "./js/GameScene.js";
+import CANNON from "cannon";
 
 const gameScene = new GameScene();
 
+let autopilot;
+let gameEnded;
+let robotPrecision; // Determines how precise the game is on autopilot
+let lastTime;
+
 const init = () => {
+  Constant.world = new CANNON.World();
+  Constant.world.gravity.set(0, -10, 0);
+  Constant.world.broadphase = new CANNON.NaiveBroadphase();
+  Constant.world.solver.iterations = 40;
+
   const canvas = document.querySelector("canvas.webgl");
   Constant.game = new THREE.Scene();
 
@@ -78,17 +89,108 @@ window.addEventListener("click", () => {
       previousLayer.threejs.position[direction];
 
     console.log("nextDirection", delta);
+
+    const overhangSize = Math.abs(delta);
+    const size = direction == "x" ? topLayer.width : topLayer.depth;
+
+    const overlap = size - overhangSize;
+
+    if (overlap > 0) {
+      //cut layer
+      const newWidth = direction == "x" ? overlap : topLayer.width;
+      const newDepth = direction == "z" ? overlap : topLayer.depth;
+
+      topLayer.width = newWidth;
+      topLayer.depth = newDepth;
+
+      //updated three.js model
+      topLayer.threejs.scale[direction] = overlap / size;
+      topLayer.threejs.position[direction] -= delta / 2;
+
+      //overhang
+      const overhangShift = (overlap / 2 + overhangSize / 2) * Math.sign(delta);
+      const overhangX =
+        direction == "x"
+          ? topLayer.threejs.position.x + overhangShift
+          : topLayer.threejs.position.x;
+
+      const overhangZ =
+        direction == "z"
+          ? topLayer.threejs.position.z + overhangShift
+          : topLayer.threejs.position.z;
+
+      const overhangWidth = direction == "x" ? overhangSize : newWidth;
+      const overhangDepth = direction == "z" ? overhangSize : newDepth;
+
+      gameScene.AddOverHang(overhangX, overhangZ, overhangWidth, overhangDepth);
+
+      //next layer
+      nextX = direction == "x" ? topLayer.threejs.position.x : -10;
+      nextZ = direction == "z" ? topLayer.threejs.position.z : -10;
+      const newDirection = "x" ? "z" : "x";
+
+      gameScene.AddLayer(nextX, nextZ, newWidth, newDepth, newDirection);
+    }
   }
 });
 
 const animation = () => {
-  const topLayer = gameScene.stack[gameScene.stack.length - 1];
-  topLayer.threejs.position[topLayer.direction] += Constant.speed;
-  if (
-    Constant.camera.position.y <
-    Constant.boxHeight * (gameScene.stack.length - 2) + 4
-  ) {
-    Constant.camera.position.y += Constant.speed;
+  if (lastTime) {
+    const timePassed = time - lastTime;
+    const speed = 0.008;
+
+    const topLayer = stack[stack.length - 1];
+    const previousLayer = stack[stack.length - 2];
+
+    // The top level box should move if the game has not ended AND
+    // it's either NOT in autopilot or it is in autopilot and the box did not yet reach the robot position
+    const boxShouldMove =
+      !gameEnded &&
+      (!autopilot ||
+        (autopilot &&
+          topLayer.threejs.position[topLayer.direction] <
+            previousLayer.threejs.position[topLayer.direction] +
+              robotPrecision));
+
+    if (boxShouldMove) {
+      // Keep the position visible on UI and the position in the model in sync
+      topLayer.threejs.position[topLayer.direction] += speed * timePassed;
+      topLayer.cannonjs.position[topLayer.direction] += speed * timePassed;
+
+      // If the box went beyond the stack then show up the fail screen
+      if (topLayer.threejs.position[topLayer.direction] > 10) {
+        missedTheSpot();
+      }
+    } else {
+      // If it shouldn't move then is it because the autopilot reached the correct position?
+      // Because if so then next level is coming
+      if (autopilot) {
+        splitBlockAndAddNextOneIfOverlaps();
+        setRobotPrecision();
+      }
+    }
+
+    // 4 is the initial camera height
+    if (camera.position.y < boxHeight * (stack.length - 2) + 4) {
+      camera.position.y += speed * timePassed;
+    }
+
+    updatePhysics(timePassed);
+    renderer.render(scene, camera);
   }
-  Constant.renderer.render(Constant.game, Constant.camera);
+  lastTime = time;
 };
+window.addEventListener("resize", () => {
+  // Adjust camera
+  console.log("resize", window.innerWidth, window.innerHeight);
+  const aspect = window.innerWidth / window.innerHeight;
+  const width = 10;
+  const height = width / aspect;
+
+  camera.top = height / 2;
+  camera.bottom = height / -2;
+
+  // Reset renderer
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+});
